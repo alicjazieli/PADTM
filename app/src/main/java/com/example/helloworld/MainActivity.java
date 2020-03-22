b3d473157a703f109afe94e727473522bbd5141c
Alicja <a.berlinska@gmail.com>
2020-03-22 16:58:57 +0100

    Lab2 - cykl, zasoby

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Lab2", "metoda onCreate");
+        Toast.makeText(this, "Metoda onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        Toast.makeText(this, "Metoda onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        Toast.makeText(this, "Metoda onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        Toast.makeText(this, "Metoda onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        Toast.makeText(this, "Metoda onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+
+        Toast.makeText(this, "Metoda onRestart", Toast.LENGTH_SHORT).show();
     }
 }
